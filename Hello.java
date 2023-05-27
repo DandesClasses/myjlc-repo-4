c4f9ae80cf6a3f28fa06b305f56660516be0487f
vasirige <43597018+vasirige@users.noreply.github.com>
2023-05-27 18:33:19 +0530

    Updating Hello for jenkins teat

--- Hello.java
+++ Hello.java
@@ -1,1 +1,8 @@
-I am Hello.java
+class Hello{
+  public static void main(String args[])
+  {
+    System.out.println("Hello Guys!!!");
+    System.out.println("Welcome to JLC. You are learning DevOps");
+    System.out.println("You are learning Jenkins here!!!! ###Happy Learning###");
+  }
+}
